16ea68f4bfbdaa9ca32b17274ce3ed685202b2c2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-06-26 16:30:42 -0300

    add migration CreateTransactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  private findOrCreateCategory(title: string): Category {
+    const categoriesRepository = getRepository(Category);
+  }
+
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    if (value <= 0) {
+      throw new AppError('Entre transaction value');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (balance.total - value < 0) {
+      throw new AppError('There is no balance for transaction');
+    }
+
+    const usedCategory = this.findOrCreateCategory(category);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: usedCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
